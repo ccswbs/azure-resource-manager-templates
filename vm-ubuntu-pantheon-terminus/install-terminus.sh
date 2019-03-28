88db7fc95314845ccac1014c1297c894a037addd
M. Brent Harp <brharp@uoguelph.ca>
2019-03-28 16:58:26 -0400

    run terminus as wsadmin

--- vm-ubuntu-pantheon-terminus/install-terminus.sh
+++ vm-ubuntu-pantheon-terminus/install-terminus.sh
@@ -5,4 +5,4 @@ sudo mkdir /opt/terminus
 cd /opt/terminus
 sudo curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar
 sudo php installer.phar install
-/usr/local/bin/terminus auth:login --machine-token=$1
+sudo -u wsadmin terminus auth:login --machine-token=$1
