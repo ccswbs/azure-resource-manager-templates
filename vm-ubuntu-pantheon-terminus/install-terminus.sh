d9e158307170c863147de86e0bcf429e051e09f0
M. Brent Harp <brharp@uoguelph.ca>
2019-03-28 15:40:17 -0400

    Machine token

--- vm-ubuntu-pantheon-terminus/install-terminus.sh
+++ vm-ubuntu-pantheon-terminus/install-terminus.sh
@@ -5,3 +5,4 @@ sudo mkdir /opt/terminus
 cd /opt/terminus
 sudo curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar
 sudo php installer.phar install
+terminus auth:login --machine-token=$1
