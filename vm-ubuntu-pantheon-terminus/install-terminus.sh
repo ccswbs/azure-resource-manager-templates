9d9477cd34bb70620e9eff64c8ba9f79a152e81d
M. Brent Harp <brharp@uoguelph.ca>
2019-05-08 12:05:26 -0400

    Generate and add ssh key

--- vm-ubuntu-pantheon-terminus/install-terminus.sh
+++ vm-ubuntu-pantheon-terminus/install-terminus.sh
@@ -6,3 +6,5 @@ cd /opt/terminus
 sudo curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar
 sudo php installer.phar install
 sudo -u wsadmin terminus auth:login --machine-token=$1
+sudo -u wsadmin ssh-keygen -f ~wsadmin/.ssh/id_rsa -N ''
+sudo -u wsadmin terminus ssh-key:add ~wsadmin/.ssh/id_rsa.pub
