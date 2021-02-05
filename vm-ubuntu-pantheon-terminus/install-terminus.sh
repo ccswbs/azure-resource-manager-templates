9c0be53fd3f1ad638afacfcd2d4cf1beb9270502
M. Brent Harp <brharp@uoguelph.ca>
2021-02-04 21:07:24 -0500

    Run terminus commands as wsadmin

--- vm-ubuntu-pantheon-terminus/install-terminus.sh
+++ vm-ubuntu-pantheon-terminus/install-terminus.sh
@@ -3,7 +3,9 @@ sudo apt update
 sudo apt -y install php-xml php-cli php-curl
 curl -L https://github.com/pantheon-systems/terminus/releases/download/$(curl --silent "https://api.github.com/repos/pantheon-systems/terminus/releases/latest" | perl -nle'print $& while m{"tag_name": "\K.*?(?=")}g')/terminus.phar --output terminus
 sudo install terminus /usr/local/bin
+sudo -i -u wsadmin sh <<HERE
 terminus auth:login --machine-token=$1
 terminus ssh-key:list --field=id | xargs -n1 terminus ssh-key:remove
 ssh-keygen -f ~wsadmin/.ssh/id_rsa -N ''
 terminus ssh-key:add ~wsadmin/.ssh/id_rsa.pub
+HERE
